18f1a8b1b3a27847f7d52e2d72a61d454c31f84a
Qwertygiy <qwertygiy@gmail.com>
2019-10-10 14:56:07 -0400

    Add call to OnChangedBlock when notifying of updates

---
+++ src/main/java/org/terasology/namegenerator/creature/species/AnimalAdjectiveAffinityVector.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.namegenerator.creature.species;
+
+import com.google.common.base.Preconditions;
+import org.terasology.namegenerator.AffinityVector;
+
+/**
+ * Defines different attribute affinities for water names
+ */
+public class AnimalAdjectiveAffinityVector implements AffinityVector {
+
+    private double prefixAffinity;
+    private double postfixAffinity;
+
+    /**
+     * Initializes default values
+     */
+    protected AnimalAdjectiveAffinityVector() {
+        // default values are 0
+
+        // avoid direct instantiation
+    }
+
+    /**
+     * @return an instance with default values
+     */
+    public static AnimalAdjectiveAffinityVector create() {
+        return new AnimalAdjectiveAffinityVector();
+    }
+
+    /**
+     * @return the prefix type affinity in [0..1]
+     */
+    public double getPrefixAffinity() {
+        return prefixAffinity;
+    }
+
+    /**
+     * @return the postfix type affinity in [0..1]
+     */
+    public double getPostfixAffinity() {
+        return postfixAffinity;
+    }
+
+    /**
+     * @param prefix the prefix in [0..1]
+     * @param postfix the postfix in [0..1]
+     * @return this
+     */
+    public AnimalAdjectiveAffinityVector type(double prefix, double postfix) {
+        Preconditions.checkArgument(prefix >= 0 && prefix <= 1, "Prefix must be in [0..1], but is " + prefix);
+        Preconditions.checkArgument(postfix >= 0 && postfix <= 1, "Postfix must be in [0..1], but is " + postfix);
+
+        this.prefixAffinity = prefix;
+        this.postfixAffinity = postfix;
+
+        return this;
+    }
+
+}
---
+++ src/main/java/org/terasology/namegenerator/creature/species/AnimalAdjectiveProvider.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.namegenerator.creature.species;
+
+import org.terasology.namegenerator.generators.MarkovNameGenerator;
+import org.terasology.namegenerator.generators.NameGenerator;
+import org.terasology.utilities.random.MersenneRandom;
+import org.terasology.utilities.random.Random;
+
+import java.util.List;
+
+public class AnimalAdjectiveProvider {
+    private final NameGenerator nameGen;
+    private final Random random;
+    private final List<String> prefixes;
+    private final List<String> postfixes;
+    private final AnimalAdjectiveAffinityVector defaultAffinity = AnimalAdjectiveAffinityVector.create();
+
+    /**
+     * Uses the default naming theme
+     *
+     * @param seed the seed
+     */
+    public AnimalAdjectiveProvider(long seed) {
+        this(seed, AnimalAdjectiveAssetTheme.ENGLISH);
+    }
+
+    /**
+     * @param seed  the seed value
+     * @param theme the naming theme
+     */
+    public AnimalAdjectiveProvider(long seed, AnimalAdjectiveTheme theme) {
+
+        random = new MersenneRandom(seed);
+
+        nameGen = new MarkovNameGenerator(seed, theme.getNames());
+        prefixes = theme.getPrefixes();
+        postfixes = theme.getPostfixes();
+    }
+
+    /**
+     * @return a creature adjective with default affinities
+     */
+    public String generateName() {
+        return generateName(defaultAffinity.type(random.nextDouble(), random.nextDouble()));
+    }
+
+    /**
+     * @param affinity the list of affinities
+     * @return the water name
+     */
+    public synchronized String generateName(AnimalAdjectiveAffinityVector affinity) {
+        if (random.nextBoolean() && random.nextBoolean() && random.nextBoolean()) return "";
+
+        String name = nameGen.nextName();
+
+        if (random.nextBoolean()) {
+            int postfixIndex = (int) (affinity.getPostfixAffinity() * postfixes.size());
+            String postfix = postfixes.get(postfixIndex);
+
+            if (random.nextBoolean()) {
+                int prefixIndex = (int) (affinity.getPrefixAffinity() * prefixes.size());
+                String prefix = prefixes.get(prefixIndex);
+                return random.nextBoolean() ? prefix + "-" + postfix : prefix + "-" + postfix + " " + name;
+            } else {
+                return name + "-" + postfix;
+            }
+        } else {
+            return name;
+        }
+    }
+}
