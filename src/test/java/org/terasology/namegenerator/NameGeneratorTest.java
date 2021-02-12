0d2132eafb0ac01ec5c2b74fcf8d47ad98ad3bb7
Michael Pollind <mpollind@gmail.com>
2021-02-11 21:05:01 -0800

    test: cleanup test cases

--- src/test/java/org/terasology/namegenerator/NameGeneratorTest.java
+++ src/test/java/org/terasology/namegenerator/NameGeneratorTest.java
@@ -1,28 +1,13 @@
-/*
- * Copyright 2013 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.namegenerator;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.terasology.namegenerator.generators.MarkovNameGenerator;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Runs a few tests on {@link MarkovNameGenerator}.
@@ -44,8 +29,8 @@ public class NameGeneratorTest {
         for (int i = 0; i < 1000; i++) {
             String name = nameGen.nextName(minLen, maxLen);
 
-            assertTrue("Length must be >= " + minLen, name.length() >= minLen);
-            assertTrue("Length must be <= " + maxLen, name.length() <= maxLen);
+            assertTrue(name.length() >= minLen, "Length must be >= " + minLen);
+            assertTrue(name.length() <= maxLen, "Length must be <= " + maxLen);
         }
     }
 
--- src/test/java/org/terasology/namegenerator/NameGeneratorTestingEnvironment.java
+++ src/test/java/org/terasology/namegenerator/NameGeneratorTestingEnvironment.java
@@ -1,23 +1,9 @@
-/*
- * Copyright 2016 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.namegenerator;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.terasology.HeadlessEnvironment;
 import org.terasology.assets.AssetFactory;
 import org.terasology.assets.management.AssetManager;
@@ -31,6 +17,7 @@ import org.terasology.entitySystem.prefab.internal.PojoPrefab;
 import org.terasology.entitySystem.prefab.internal.PrefabFormat;
 import org.terasology.naming.Name;
 import org.terasology.persistence.typeHandling.TypeHandlerLibrary;
+import org.junit.jupiter.api.Test;
 
 public class NameGeneratorTestingEnvironment {
     private static HeadlessEnvironment env;
@@ -38,7 +25,7 @@ public class NameGeneratorTestingEnvironment {
     /**
      * Setup headless environment
      */
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
         env = new HeadlessEnvironment(new Name("NameGenerator"));
 
@@ -63,7 +50,7 @@ public class NameGeneratorTestingEnvironment {
      * Clean up
      * @throws Exception never
      */
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() throws Exception {
         env.close();
     }
