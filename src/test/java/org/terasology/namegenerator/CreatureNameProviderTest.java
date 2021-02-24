afc8b47ed2a8597b8d9ff8e4668fb4b490ff3136
Michael Pollind <mpollind@gmail.com>
2021-02-24 07:40:06 -0800

    test: migrate tests to JUnit 5 / Jupiter (#27)

--- src/test/java/org/terasology/namegenerator/CreatureNameProviderTest.java
+++ src/test/java/org/terasology/namegenerator/CreatureNameProviderTest.java
@@ -1,31 +1,18 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.namegenerator;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.terasology.HeadlessEnvironment;
 import org.terasology.namegenerator.creature.CreatureAffinityVector;
 import org.terasology.namegenerator.creature.CreatureNameProvider;
 import org.terasology.naming.Name;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * Tests {@link CreatureNameProvider}
  */
@@ -36,16 +23,17 @@ public class CreatureNameProviderTest {
     /**
      * Setup headless environment
      */
-    @BeforeClass
+    @BeforeAll
     public static void setUpClass() {
         env = new HeadlessEnvironment(new Name("NameGenerator"));
     }
 
     /**
      * Clean up
+     *
      * @throws Exception never
      */
-    @AfterClass
+    @AfterAll
     public static void tearDownClass() throws Exception {
         env.close();
     }
@@ -66,8 +54,7 @@ public class CreatureNameProviderTest {
         for (int i = 0; i < 100; i++) {
             CreatureAffinityVector aff = CreatureAffinityVector.create().maleOnly().nobility(1);
             String name = prov.generateName(aff);
-            assertTrue("Name \"" + name + "\" has not the form <FIRST> <LAST> the <ATTR>", name.matches("[\\S]+ [\\S]+ the [\\S]+"));
+            assertTrue(name.matches("[\\S]+ [\\S]+ the [\\S]+"), "Name \"" + name + "\" has not the form <FIRST> <LAST> the <ATTR>");
         }
     }
-
 }
--- src/test/java/org/terasology/namegenerator/TownNameProviderTest.java
+++ src/test/java/org/terasology/namegenerator/TownNameProviderTest.java
@@ -1,29 +1,17 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.namegenerator;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.namegenerator.town.TownAffinityVector;
 import org.terasology.namegenerator.town.TownNameProvider;
 
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Tests {@link TownNameProvider}
@@ -48,13 +36,13 @@ public class TownNameProviderTest extends NameGeneratorTestingEnvironment {
         for (int i = 0; i < 1000; i++) {
             TownAffinityVector aff = TownAffinityVector.create().prefix(1);
             String name = townNameProv.generateName(aff);
-            assertTrue("The name \"" + name + "\" does not contain an affix", name.contains(" "));
+            assertTrue(name.contains(" "), "The name \"" + name + "\" does not contain an affix");
         }
 
         for (int i = 0; i < 1000; i++) {
             TownAffinityVector aff = TownAffinityVector.create().postfix(1);
             String name = townNameProv.generateName(aff);
-            assertTrue("The name \"" + name + "\" does not contain an affix", name.contains(" "));
+            assertTrue(name.contains(" "), "The name \"" + name + "\" does not contain an affix");
         }
     }
 
@@ -101,16 +89,20 @@ public class TownNameProviderTest extends NameGeneratorTestingEnvironment {
     /**
      * Single affix > 1
      */
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testInvalidArgs1() {
-        TownAffinityVector.create().prefix(1.5);
+        assertThrows(IllegalArgumentException.class, () -> {
+            TownAffinityVector.create().prefix(1.5);
+        });
     }
 
     /**
      * Combined affixes > 1
      */
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testInvalidArgs2() {
-        TownAffinityVector.create().prefix(0.5).postfix(0.7);
+        assertThrows(IllegalArgumentException.class, () -> {
+            TownAffinityVector.create().prefix(0.5).postfix(0.7);
+        });
     }
 }
